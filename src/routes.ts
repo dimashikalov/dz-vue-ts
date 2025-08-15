54a0460841ce53e89a526b4cf33e2a1a20fdae75
dimashikalov <dimashikalov86@mail.ru>
2025-08-15 17:55:35 +0300

    доработал Button + добавил vue-router

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+export const router = createRouter({
+  routes: [
+    { path: '/', component: () => import('./views/AuthView.vue') },
+    { path: '/main', component: () => import('./views/MainView.vue'), name: 'main' },
+  ],
+  history: createWebHistory(),
+});
---
+++ src/views/MainView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MeditationList from '@/components/MeditationList.vue';
+</script>
+
+<template>
+  <h1>Приложение для медитации</h1>
+  <MeditationList />
+</template>
+
+<style scoped></style>
